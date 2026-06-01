06fae5fc1f1e4f177c5541f1913e0b1602b5209a
Jayyy-mark <jaycoab2@gmail.com>
2026-06-01 11:23:44 +0630

    update on context/AuthContent.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import api from "../helpers/api";
 
 type User = {
   id: number;
@@ -6,7 +7,7 @@ type User = {
   username: string;
   email: string;
   role: string;
-  staff_id:number;
+  staff_id: number;
 };
 
 type AuthContextType = {
@@ -20,28 +21,21 @@ export const useAuth = () => useContext(AuthContext);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState(true); // ⬅️ new
+  const [loading, setLoading] = useState(true);
 
   const logout = async () => {
-    await fetch("http://localhost:8000/api/auth/logout/", {
-      method: "POST",
-      credentials: "include",
-    });
+    await api.post("auth/logout/");
     setUser(null);
   };
 
   const fetchCurrentUser = async () => {
     try {
-      const res = await fetch("http://localhost:8000/api/auth/user/", {
-        credentials: "include",
-      });
-      if (!res.ok) throw new Error();
-      const data = await res.json();
+      const { data } = await api.get("auth/user/");
       setUser(data);
     } catch {
       setUser(null);
     } finally {
-      setLoading(false); // ⬅️ finished fetching
+      setLoading(false);
     }
   };
 
@@ -49,7 +43,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     fetchCurrentUser();
   }, []);
 
-  if (loading) return <div>Loading...</div>; // ⬅️ prevent redirect before fetch
+  if (loading) return <div>Loading...</div>;
 
   return (
     <AuthContext.Provider value={{ user, setUser, logout }}>
