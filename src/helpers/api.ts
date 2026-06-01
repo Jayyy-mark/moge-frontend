52aab214f8c595ad8bfed5dd06a9f42d28a3bc29
Jayyy-mark <jaycoab2@gmail.com>
2026-06-01 10:46:31 +0630

    update on api network helper/api.ts

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    //baseURL: "http://localhost:8000/api/",
+    baseURL:"https://moge-backend.up.railway.app/api/",
     withCredentials: true,
 });
 
